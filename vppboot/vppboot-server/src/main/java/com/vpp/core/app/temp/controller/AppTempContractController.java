7e89166f828300532491badba45a132e1d5119c6
ValPromise官方 <36215421+ValPromise@users.noreply.github.com>
2018-06-29 17:57:36 +0800

    Update AppTempContractController.java

--- vppboot/vppboot-server/src/main/java/com/vpp/core/app/temp/controller/AppTempContractController.java
+++ vppboot/vppboot-server/src/main/java/com/vpp/core/app/temp/controller/AppTempContractController.java
@@ -1,10 +1,12 @@
 package com.vpp.core.app.temp.controller;
 
 import java.text.MessageFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.concurrent.TimeUnit;
 
 import javax.servlet.http.HttpServletResponse;
 
@@ -12,6 +14,7 @@ import org.apache.commons.lang3.StringUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
@@ -21,6 +24,8 @@ import com.vpp.common.utils.DateUtil;
 import com.vpp.common.vo.ResultVo;
 import com.vpp.core.common.CommonController;
 import com.vpp.core.standardized.order.service.IContractService;
+import com.vpp.core.weather.bean.WeatherData;
+import com.vpp.core.weather.service.IWeatherService;
 
 @RestController
 @RequestMapping("/app/temp/contract")
@@ -29,6 +34,20 @@ public class AppTempContractController extends CommonController {
 
     @Autowired
     private IContractService contractService;
+
+    @Autowired
+    private IWeatherService weatherService;
+    @Autowired
+    private RedisTemplate<String, Object> redisTemplate;
+
+    /**
+     * 温度历史数据key
+     */
+    public static final String TEMP_HISTORY_KEY = "temp_history_";
+    /**
+     * 降雨历史数据key
+     */
+    public static final String RAIN_HISTORY_KEY = "rain_history_";
 
     @RequestMapping(value = "/getContract")
     @ResponseBody
@@ -63,6 +82,8 @@ public class AppTempContractController extends CommonController {
                 contractService.getCityNameByCityId(map.get("city_id").toString()));
         contract.put("contractTitle", contractTitle);
         contract.put("maxBuyCount", ConstantsTemp.MAX_BUY_COUNT);
+        contract.put("history", this.tempHistory(cityId));
+
         return ResultVo.setResultSuccess(contract);
     }
 
@@ -98,7 +119,41 @@ public class AppTempContractController extends CommonController {
                 DateUtil.strToStr(map.get("date_ref").toString(), DateUtil.YMD_DATE_TIME_PATTERN, DateUtil.YMD_DATE_PATTERN),
                 contractService.getCityNameByCityId(map.get("city_id").toString()));
         contract.put("contractTitle", contractTitle);
-        contract.put("maxBuyCount", ConstantsTemp.MAX_BUY_COUNT);//最大购买份数
+        contract.put("maxBuyCount", ConstantsTemp.MAX_BUY_COUNT);// 最大购买份数
+        contract.put("history", this.tempHistory(cityList.get(0)));
         return ResultVo.setResultSuccess(contract);
+    }
+
+    /**
+     * 天气行情
+     * 
+     * @author Lxl
+     * @param cityId
+     * @return
+     */
+    private List<Map<String, Object>> tempHistory(String cityId) {
+        List<Map<String, Object>> list = new ArrayList<Map<String, Object>>();
+        try {
+            String key = TEMP_HISTORY_KEY + cityId;
+            // 缓存
+            if (redisTemplate.hasKey(key)) {
+                list = (List<Map<String, Object>>) redisTemplate.opsForValue().get(key);
+                return list;
+            }
+            Date eDate = DateUtil.getYesterday();
+            Date sDate = DateUtil.diffDate(eDate, 7);
+            List<String> dates = DateUtil.getDatesBetweenTwoDate(sDate, eDate, DateUtil.YMD_DATE_TIME_PATTERN);
+            for (String date : dates) {
+                WeatherData weatherData = weatherService.selectWeatherTemp(cityId, date);
+                Float maxTemp = weatherData.getRealMaxTemp();
+                Map<String, Object> data = new HashMap<String, Object>();
+                data.put("type", date.substring(date.length() - 2));
+                data.put("value", maxTemp);
+                list.add(data);
+            }
+            redisTemplate.opsForValue().set(TEMP_HISTORY_KEY + cityId, list, 1, TimeUnit.HOURS);
+        } catch (Exception e) {
+        }
+        return list;
     }
 }
