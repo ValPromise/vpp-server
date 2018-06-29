f9c5fa8005d79896d004a68e65f71e46b3c56857
ValPromise官方 <36215421+ValPromise@users.noreply.github.com>
2018-06-29 17:46:52 +0800

    帮助信息和广告参数化

--- vppboot/vppboot-server/src/main/java/com/vpp/core/standardized/productcoinguess/app/ProductCoinguessController.java
+++ vppboot/vppboot-server/src/main/java/com/vpp/core/standardized/productcoinguess/app/ProductCoinguessController.java
@@ -6,6 +6,8 @@ import com.vpp.core.common.CommonController;
 import com.vpp.core.standardized.productcoinguess.bean.ProductCoinguess;
 import com.vpp.core.standardized.productcoinguess.bean.ProductCoinguessList;
 import com.vpp.core.standardized.productcoinguess.service.IProductCoinguessService;
+import com.vpp.core.systemparam.bean.SystemParam;
+import com.vpp.core.systemparam.service.ISystemParamService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,15 +24,23 @@ public class ProductCoinguessController extends CommonController {
     @Autowired
     private IProductCoinguessService productCoinguessService;
 
+    @Autowired
+    private ISystemParamService systemParamService;
+
     @RequestMapping(value = "/getList")
     public ResultVo getProductCoinguessList(String token, HttpServletResponse response) {
         response.addHeader("Access-Control-Allow-Origin", "*");
 
         List<ProductCoinguessList> list = productCoinguessService.selectProductCoinguessByStatus();
+
+        //读system参数
+        SystemParam systemParamHelp = systemParamService.selectByParamTypeAndName("coinguess","coinguess_help");
+        SystemParam systemParamAd = systemParamService.selectByParamTypeAndName("coinguess","coinguess_ad");
+
         Map<String, Object> result = new HashMap<String, Object>();
-        result.put("help","合约规则：\n1.以60s移动价格均线上所选价格为下单价格，在系统允许次数内可重复下单\n2.合约到期时，系统以来自交易所的币价与用户下单价格对比，如果用户预判正确，则获得下单金额180%补偿\n3.如有疑问，请APP内用户反馈留言，或加微信valpromise-vpp\n注：60s移动价格均线上任意一点的值＝该点之前60s价格平均值");
-        result.put("ad","公募认购倒计时！详情添加微信小助理：valpromise-vpp");
         result.put("product",list);
+        result.put("help",systemParamHelp.getParamValue());
+        result.put("ad",systemParamAd.getParamValue());
         return ResultVo.setResultSuccess(result);
     }
 
