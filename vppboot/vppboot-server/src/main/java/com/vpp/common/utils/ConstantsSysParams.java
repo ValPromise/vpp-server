b81f3422cdd678fa47fe273b9dad5151b18de9e4
ValPromise官方 <36215421+ValPromise@users.noreply.github.com>
2018-07-09 14:31:19 +0800

    Create ConstantsSysParams.java

---
+++ vppboot/vppboot-server/src/main/java/com/vpp/common/utils/ConstantsSysParams.java
@@ -0,0 +1,51 @@
+package com.vpp.common.utils;
+
+/**
+ * 系统参数常量
+ * 
+ * @author Lxl
+ * @version V1.0 2018年4月18日
+ */
+public interface ConstantsSysParams {
+
+    /**
+     * 天气实况帮助链接 http://www.baotianqi.cn/qa.html
+     */
+    public static final String WEATHER_HELP_URL = "weather_help_url";
+
+    public static final String SHARE_TITLE = "share_title";
+    public static final String SHARE_CONTENT = "share_content";
+    /**
+     * app默认显示logo图标
+     */
+    public static final String SHARE_ICON = "share_icon";//
+    public static final String SHARE_URL = "share_url";
+
+    /**
+     * 最小提现金额
+     */
+    public static final String WITHDRAWAL_AMOUNT_MIN = "withdrawal_amount_min";
+
+    /**
+     * 邀请客户赠送VPP数 30
+     */
+    public static final String CUSTOMER_INVITE_ADD_VPP = "customer_invite_add_vpp";
+    /**
+     * 注册赠送VPP数 30
+     */
+    public static final String CUSTOMER_REGISTER_ADD_VPP = "customer_register_add_vpp";
+    /**
+     * 客户邀请人数上限
+     */
+    public static final String CUSTOMER_INVITE_UP = "customer_invite_up";
+
+    /**
+     * 客户签到赠送vpp
+     */
+    public static final String CUSTOMER_SIGN_IN_ADD_VPP = "customer_sign_in_add_vpp";
+
+    /**
+     * 提现手续费单位（GW）
+     */
+    public static final String GAS_PRICE = "gas_price";
+}
